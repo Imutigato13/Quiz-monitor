ffc1c2399a118dfeabf9f342f5f4a105812ba1de
MZ <mario.zambrano@udea.edu.co>
2024-03-13 11:23:46 -0500

    Se elimino archivo de pruebas

--- Pruebas.py
+++
@@ -1,5 +0,0 @@
-import datetime as datetime
-from validaciones import *
-fecha_mantenimiento = datetime.datetime.strptime(validar_alfabetico("Ingrese la fecha de mantenimiento en formato DD/MM/YY: "), '%d/%m/%Y')
-#fecha_revision = datetime.datetime.now().strftime("%d/%m/%Y")
-print(fecha_mantenimiento)
